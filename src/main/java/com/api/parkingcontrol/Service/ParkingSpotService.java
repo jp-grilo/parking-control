fe89cbc3b45307c0563045d9ef9cda2a81a7c454
joao.grilo <joao.grilo@meta.com.br>
2022-10-27 19:55:32 -0300

    Controller, service, repository and basic model created

---
+++ src/main/java/com/api/parkingcontrol/Service/ParkingSpotService.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.Service;
+
+import com.api.parkingcontrol.Respository.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+}
