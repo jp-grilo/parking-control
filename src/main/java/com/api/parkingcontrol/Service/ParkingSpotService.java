121b18f944235ad24b18c1160a566de4289e5f36
joao.grilo <joao.grilo@meta.com.br>
2022-10-29 10:46:09 -0300

    Get method created

--- src/main/java/com/api/parkingcontrol/Service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/Service/ParkingSpotService.java
@@ -5,6 +5,7 @@ import com.api.parkingcontrol.Respository.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class ParkingSpotService {
@@ -28,5 +29,9 @@ public class ParkingSpotService {
     }
     public boolean existByApartmentAndBlock(String apartment, String block){
         return parkingSpotRepository.existsByApartmentAndBlock(apartment,block);
+    }
+
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
     }
 }
